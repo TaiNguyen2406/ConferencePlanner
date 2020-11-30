7b56ccb0d90b3e81083aee9f76a96098d293cb52
Tai Nguyen <Tai.Nguyen@episerver.com>
2020-11-30 13:38:17 +0700

    Add project files.

---
+++ GraphQL/AddSpeakerPayload.cs
@@ -0,0 +1,18 @@
+﻿using ConferencePlanner.GraphQL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferencePlanner.GraphQL
+{
+    public class AddSpeakerPayload
+    {
+        public AddSpeakerPayload(Speaker speaker)
+        {
+            Speaker = speaker;
+        }
+
+        public Speaker Speaker { get; }
+    }
+}
---
+++ GraphQL/Query.cs
@@ -0,0 +1,12 @@
+﻿using ConferencePlanner.GraphQL.Data;
+using HotChocolate;
+using System.Linq;
+
+namespace ConferencePlanner.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) =>
+            context.Speakers;
+    }
+}
